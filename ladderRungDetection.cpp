0002e83750c0cac0f7bd49eed34a588c3439a6fd
darwin <vleong1@e.ntu.edu.sg>
2013-03-12 10:12:43 +0800

    update

--- ladderRungDetection.cpp
+++ ladderRungDetection.cpp
@@ -10,15 +10,10 @@ Mat original;//hold e image
 Mat inrange;//filter out only white color to grayscale single channel array
 Mat pyr, timg, gray;
 
-vector<vector<Point> > contours;
-//declaration of a vector of a vector of X and Y points
-//the nested vector of points is the list of coordinates of point that make up ONE contour
-//the outer vector of vectors of points is the list of ALL the contours detected
-
 vector<vector<Point> > squares;
-//a vector storing a vector storing a list of X and Y points which refer to the vertices of each detected rectangle
+vector<vector<Point> > contours;
 vector<Point> approx;
-//a vector to store a list of vertices of ONE detected rectangle
+//vector<vector<Point> > coordinates;
 
 static double angle( Point pt1, Point pt2, Point pt0 )
 {
@@ -31,58 +26,24 @@ static double angle( Point pt1, Point pt2, Point pt0 )
 
 static void findSquares( const Mat& image, vector<vector<Point> >& squares )
 {
-
-    squares.clear();
-	//empty the previous stored value
+    squares.clear();//empty the previous stored value
 
 	int squareCount=0;
 	  
 	Canny(image, gray, 50, 200, 5);
-	//Detect edges on the input "image" passed by reference from the main program into this findSquare function
-	//save the newly processed to "gray"
-	//with lower threshold of 50 and higher threshold to 200
-	//with the aperture size of the kernel at 5
-
 	dilate(gray, gray, Mat(), Point(-1,-1));
-	//take input image, that is the 1st "gray"
-	//"blur" the image (I am not too sure of the term that should be used)
-	//save processed image to 2nd "gray"
-	//using the kernel of default aperture size of 3 as defined by "Mat()"
-	//at default start point specified by "Point(-1,-1)"
 
     findContours(gray, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-	//input image is "gray"
-	//processed image is saved to contours
-	//in the designated mode
 
     for( size_t i = 0; i < contours.size(); i++ )
     {
         approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
-		//for each detected contour, if they approximate to a polygon according to Ramer–Douglas–Peucker algorithm
-		//the vertices of X and Y coordinates are stored as a list in the vector "approx"
-
-		//if the polygons are squares or rectangles they must be
-        if( approx.size() == 4							//4sided
-			&& fabs(contourArea(Mat(approx))) > 1000	//big enough (eliminate noise)
-			&& isContourConvex(Mat(approx)) )			//convex
+	
+        if( approx.size() == 4 && fabs(contourArea(Mat(approx))) > 1000 && isContourConvex(Mat(approx)) )// and be convex.
         {
             squareCount++;
-			//taking count of the number of rectangles detected
-
-			double maxCosine = 0;			
-            for( int j = 2; j < 5; j++ )
-            {
-                double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
-				//store cosine of the angle between 3 adjacent vertices into the variable "cosine"
-                maxCosine = MAX(maxCosine, cosine);
-            }
-
-            if( maxCosine < 0.3 )
-				squares.push_back(approx);
-				//if the angles are above cos^-1(0.3) = 72 degrees,
-				//the 4 sided polygon characterized by the current contour is considered a square
-				//the "push_back" function stores vector of 4 2D points into the "squares" vector
-
+			double maxCosine = 0;
+			//printing the coordinates of each square
 			double sum_x=0, sum_y=0;
 			for(int k=0; k<approx.size(); k++)
 			{
@@ -90,7 +51,16 @@ static void findSquares( const Mat& image, vector<vector<Point> >& squares )
 				sum_y+=approx[k].y;
 			}
 			cout << "Square #"<< squareCount << "\nX: "<< sum_x/4 << "\t\tY: " << sum_y/4 <<endl;
-			//print the coordinates of the center of each detected rectangles
+			///////////
+            for( int j = 2; j < 5; j++ )
+            {
+                double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
+                maxCosine = MAX(maxCosine, cosine);
+            }
+
+            if( maxCosine < 0.3 )
+                squares.push_back(approx);
+				//each time push_back() stores 4 2D points into squares
         }
     }
 }
@@ -102,8 +72,10 @@ static void drawSquares( Mat& image, const vector<vector<Point> >& squares )
         const Point* p = &squares[i][0];
         int n = (int)squares[i].size();
         polylines(image, &p, &n, 1, true, Scalar(0,255,0), 3, CV_AA);
-		//outline each detected rectangles
+		//C: void cvPolyLine(CvArr* img, CvPoint** pts, const int* npts, int contours, int is_closed, CvScalar color, int thickness=1, int line_type=8, int shift=0 )
     }
+
+    //imshow(wndname, image);
 }
 
 int main(int argc, char** argv)
@@ -115,15 +87,15 @@ int main(int argc, char** argv)
     while(1)
     {
 		cap >> original; // get a new frame from camera
-		
-		inRange(timg,// input timg as the image to be processed
-				Scalar(0,  0,  0),// min filtering value (if color is greater than or equal to this)
-				Scalar(90,90,90),// max filtering value (if color is less than this)
-				inrange);//save filtered image to inrange
 
 		// down-scale and upscale the image to filter out the noise
-		pyrDown(inrange, pyr, Size(original.cols/2, original.rows/2));
+		pyrDown(original, pyr, Size(original.cols/2, original.rows/2));
 		pyrUp(pyr, timg, original.size());
+		//cvtColor(timg, inrange, CV_BGR2GRAY);
+		inRange(	timg,				// function input
+					Scalar(0,  0,  0),			// min filtering value (if color is greater than or equal to this)
+					Scalar(90,90,90),			// max filtering value (if color is less than this)
+					inrange);
 
 		findSquares(inrange,squares);
 		drawSquares(original,squares);
